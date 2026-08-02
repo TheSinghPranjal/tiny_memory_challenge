f71c9d2aeae1f4f09ed6e02a08171115f936f0dd
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-08-02 14:57:14 +0530

    fix : settings fixes

---
+++ lib/core/navigation/app_route_observer.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+/// Used by [HomeScreen] to restart loop music when it becomes visible again.
+final RouteObserver<ModalRoute<void>> appRouteObserver =
+    RouteObserver<ModalRoute<void>>();
