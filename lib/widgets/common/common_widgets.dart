9b33ec7f583dbc2777b05a7e0160b8b540358021
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-08-04 16:56:07 +0530

    fix : fixes

--- lib/widgets/common/common_widgets.dart
+++ lib/widgets/common/common_widgets.dart
@@ -116,6 +116,8 @@ class _PrimaryGameButtonState extends State<PrimaryGameButton>
     final glowColor = widget.glowColor ?? AppColors.secondary;
 
 
+
+
     final button = Container(
       width: widget.width,
       constraints: const BoxConstraints(minHeight: 56, minWidth: 160),
